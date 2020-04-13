8d7e415dd10f5ede5c239509f3962c4757280639
Sarthak Johnson Prasad <sarthakjohnsonprasad@gmail.com>
2020-04-13 15:43:10 +0530

    Increase rank to 500 and Add score

--- top-anime-list.py
+++ top-anime-list.py
@@ -1,17 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://myanimelist.net/topanime.php")
-soup = BeautifulSoup(response.content,'lxml')
-
-datas = soup.find_all("div", class_ = "detail")
-scores = soup.find_all("div", class_= "js-top-ranking-score-col di-ib al")
 i=1
-for data in datas :
-    print(i,end ="") 
-    print(". ",end ="")
-    print(data.find("a",class_= "hoverinfo_trigger fl-l fs14 fw-b").text.replace("\n",""))
-    print(data.find("div",class_="information di-ib mt4").text.replace("\n",""))
-    print("\n")
-    i= i+1
-    # print(data.find_all("div", class_="information di-ib mt4")).text
+baseUrl = "https://myanimelist.net/topanime.php?limit="
+
+for iterator in range (0,10,1) :
+    response = requests.get(baseUrl + str(iterator*50))
+    soup = BeautifulSoup(response.content,'lxml')
+    ranking_list = soup.find_all("tr",class_= "ranking-list")
+
+    for data in ranking_list:
+        print(i,end ="") 
+        print(". ",end ="")
+        print(data.find("div", class_ = "detail").find("a",class_= "hoverinfo_trigger fl-l fs14 fw-b").text.replace("\n",""))
+        print(data.find("div", class_ = "detail").find("div",class_="information di-ib mt4").text.replace("\n",""))
+        print("        Score: " + data.find("div", class_= "js-top-ranking-score-col di-ib al").find("span").text.replace("\n",""))
+        print("\n")
+        i= i+1
