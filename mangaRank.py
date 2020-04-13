27eb1b3e4902bb208320645d41368d649d935b59
Ashish Gokarnkar <ashish.gokarnkar@iitkgp.ac.in>
2020-04-13 22:02:23 +0530

    Comments added

--- mangaRank.py
+++ mangaRank.py
@@ -2,17 +2,19 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://myanimelist.net/topmanga.php")
+rank = number = int(input("Enter the RANK :: "))
+digits = 0
+while(number > 0):
+    number = number // 10
+    digits = digits + 1
+rank_page = str('lightLink top-anime-rank-text rank' + str(digits))
+
+limit = str(50 * int(rank / 50))
+r = requests.get("https://myanimelist.net/topmanga.php?limit=" + limit)
 c = r.content
 soup = BeautifulSoup(c, "html.parser")
 ranking_list = soup.find_all("tr", {"class" : "ranking-list"})
 
-rank = int(input("Enter the RANK :: "))
-if rank in range(0,9):
-    rank_page = str('lightLink top-anime-rank-text rank1')
-elif rank in range(10,50):
-    rank_page = str('lightLink top-anime-rank-text rank2')
-
-print('Name :: ' + ranking_list[rank-1].find("a", "hoverinfo_trigger fs14 fw-b").text)
-print('Volumes :: ' + ranking_list[rank-1].find("div", "information di-ib mt4").text)
-print('Score :: ' + ranking_list[rank-1].find("div", "js-top-ranking-score-col di-ib al").find("span").text)
+print('Name :: ' + ranking_list[(rank-1) % 50].find("a", "hoverinfo_trigger fs14 fw-b").text)
+print('Volumes :: ' + ranking_list[(rank-1) % 50].find("div", "information di-ib mt4").text)
+print('Score :: ' + ranking_list[(rank-1) % 50].find("div", "js-top-ranking-score-col di-ib al").find("span").text)
